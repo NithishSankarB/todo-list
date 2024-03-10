6733d2da8f5fbd417915552b676b4a7d624812b1
NithishSankarB <106070269+NithishSankarB@users.noreply.github.com>
2024-03-10 10:47:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,10 +38,5 @@ app.get('/todo/complete/:id',async(req,res)=>{
     res.json(todo);
 })
 
-app.put('/todo/update/:id',async(req,res)=>{
-    const todo=await Todo.findById(req.params.id);
-    todo.text=req.body.text;
-    todo.save();
-    res.json(todo);
-})
+
 
